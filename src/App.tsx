d181e0ee806a5e68aca475403d5616ef3a30483b
Fatima Zahra Dhaouadi <107303242+FatimaZahraDhaouadi@users.noreply.github.com>
2024-10-26 15:29:59 +0200

    Init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+//import reactLogo from './assets/react.svg' //path relativo al file dove viene fatto l'import
+//import viteLogo from '/vite.svg' //PATH ASSOLUTO
+import './App.css'
+/*Esistono per meccanisco = lib React -componente è funzione che ritorna jsx(interfaccia)/tsx- */ 
+/*Ogni funzione tira fuori un Nodo*/
+/*Fragment = uso x wrapp tit e subtit */
+function App() {
+
+  return (
+   <>  
+   </>
+  )
+}
+
+export default App
